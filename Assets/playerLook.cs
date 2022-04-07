7f94c48da333fc09b1890995c457a73e63292449
Alfie <87566534+Frosty11234@users.noreply.github.com>
2022-04-07 23:05:21 +0100

    Added full item movement system

--- Assets/playerLook.cs
+++ Assets/playerLook.cs
@@ -7,17 +7,16 @@ public class playerLook : MonoBehaviour
     // Start is called before the first frame update
 
     public LayerMask selectionPointLayer;
+    public GameObject itemSnap;
     RaycastHit hit;
-    bool temp = true;
-
-    void Start()
-    {
-        
-    }
+    private bool temp = true;
+    private bool holdingItem = false;
+    private GameObject heldItem;
 
     // Update is called once per frame
     void Update()
     {
+        //debug draw rays with their colours
         if(temp)
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100f, Color.green);
@@ -28,17 +27,56 @@ public class playerLook : MonoBehaviour
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.blue);
         }
 
+
+
+        //pickup item and raycast
         if(Input.GetKeyDown("e"))
         {
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10f, selectionPointLayer))
+            if(!holdingItem)
             {
-                temp = false;
-                Debug.Log("Did Hit");
-                Invoke("ColourReset", 1f);
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10f, selectionPointLayer))
+                {
+
+                    //colour debug
+                    temp = false;
+                    Debug.Log("Did Hit");
+                    Invoke("ColourReset", 1f);
+
+                    //picks up the item
+                    if(hit.transform.tag == "Moveable")
+                    {
+                        heldItem = hit.transform.gameObject;
+                        holdingItem = true;
+                    }
+                }
             }
+            else
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10f, selectionPointLayer))
+                {
+                    //colour debug
+                    temp = false;
+                    Debug.Log("Did Hit");
+                    Invoke("ColourReset", 1f);
+                    
+                    //moves the item to the snap point
+                    if(hit.transform.tag == "SnapPoint")
+                    {
+                        holdingItem = false;
+                        heldItem.transform.position = hit.transform.position;
+                    }
+                }
+            }
+        }
+
+        //move item to the players snap point
+        if(holdingItem)
+        {
+            heldItem.transform.position = itemSnap.transform.position;
         }
     }
 
+    //reset the debug colour
     void ColourReset(){
         temp = true;
     }
